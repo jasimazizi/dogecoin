b7dadb53f676cb52ec35b28f21a2533114e15c88
Jasim Azizi <jasim.azizi@gmail.com>
2023-03-04 23:35:05 -0600

    LaunchScreen (Dogecoin)

---
+++ Dogecoin/LaunchScreen/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  LaunchScreen
+//
+//  Created by Jasim Azizi
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    /// A light status bar, intended for use on dark backgrounds.
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+}
